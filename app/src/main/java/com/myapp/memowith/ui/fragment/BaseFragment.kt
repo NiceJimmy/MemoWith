1e89e75452bde5eb572c39f7c8a1d94fe342438d
이광우 <pebteam2014@gmail.com>
2022-05-19 00:58:49 +0900

    #dev databinding 기능 적용.

---
+++ app/src/main/java/com/myapp/memowith/ui/fragment/BaseFragment.kt
@@ -0,0 +1,8 @@
+package com.myapp.memowith.ui.fragment
+
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<B : ViewDataBinding> : Fragment(){
+
+}
